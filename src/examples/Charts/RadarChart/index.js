ff225dad6e6c26f246f4656be164b33ef24a6318
prajein <prajeinck@gmail.com>
2024-05-11 16:25:54 +0530

    created additional reusable components

---
+++ src/examples/Charts/RadarChart/index.js
@@ -0,0 +1,109 @@
+import { useMemo } from "react";
+import PropTypes from "prop-types";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
+import Card from "@mui/material/Card";
+import Icon from "@mui/material/Icon";
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import configs from "examples/Charts/RadarChart/configs";
+import colors from "assets/theme/base/colors";
+import rgba from "assets/theme/functions/rgba";
+
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
+
+function RadarChart({ icon, title, description, height, chart }) {
+  const chartDatasets = chart.datasets
+    ? chart.datasets.map((dataset) => ({
+        ...dataset,
+        backgroundColor: colors[dataset.color]
+          ? rgba(colors[dataset.color || "dark"].main, 0.2)
+          : rgba(colors.dark.main, 0.2),
+      }))
+    : [];
+
+  const { data, options } = configs(chart.labels || [], chartDatasets);
+
+  const renderChart = (
+    <MDBox py={2} pr={2} pl={icon.component ? 1 : 2}>
+      {title || description ? (
+        <MDBox display="flex" px={description ? 1 : 0} pt={description ? 1 : 0}>
+          {icon.component && (
+            <MDBox
+              width="4rem"
+              height="4rem"
+              bgColor={icon.color || "dark"}
+              variant="gradient"
+              coloredShadow={icon.color || "dark"}
+              borderRadius="xl"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              color="white"
+              mt={-5}
+              mr={2}
+            >
+              <Icon fontSize="medium">{icon.component}</Icon>
+            </MDBox>
+          )}
+          <MDBox mt={icon.component ? -2 : 0}>
+            {title && <MDTypography variant="h6">{title}</MDTypography>}
+            <MDBox mb={2}>
+              <MDTypography component="div" variant="button" color="text">
+                {description}
+              </MDTypography>
+            </MDBox>
+          </MDBox>
+        </MDBox>
+      ) : null}
+      {useMemo(
+        () => (
+          <MDBox p={6} height={height}>
+            <Radar data={data} options={options} redraw />
+          </MDBox>
+        ),
+        [chart]
+      )}
+    </MDBox>
+  );
+
+  return title || description ? <Card>{renderChart}</Card> : renderChart;
+}
+
+// Setting default values for the props of RadarChart
+RadarChart.defaultProps = {
+  icon: { color: "info", component: "" },
+  title: "",
+  description: "",
+};
+
+// Typechecking props for the RadarChart
+RadarChart.propTypes = {
+  icon: PropTypes.shape({
+    color: PropTypes.oneOf([
+      "primary",
+      "secondary",
+      "info",
+      "success",
+      "warning",
+      "error",
+      "light",
+      "dark",
+    ]),
+    component: PropTypes.node,
+  }),
+  title: PropTypes.string,
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  chart: PropTypes.objectOf(PropTypes.array).isRequired,
+};
+
+export default RadarChart;
