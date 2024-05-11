7efe46fe2d9693504d206bfeea5428b9f66dddb2
prajein <prajeinck@gmail.com>
2024-05-12 01:23:26 +0530

    boilerplate dashboard was created

---
+++ src/layouts/dashboardbank/index.js
@@ -0,0 +1,117 @@
+import Grid from "@mui/material/Grid";
+import MDBox from "components/MDBox";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import SMEFillCard from "./components/SMEFillCard";
+import Card from "@mui/material/Card";
+import MDTypography from "components/MDTypography";
+import DataTable from "examples/Tables/DataTable";
+import loanReqTableData from "layouts/tables/data/loanReqTableData";
+import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
+
+
+
+function DashboardBank() {
+  const { columns, rows } = loanReqTableData();
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Loan Requests
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+      <MDBox mt={2} py={3}>
+        
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="dark"
+                icon="person_add"
+                title="Total Requests"
+                count={281}
+                percentage={{
+                  color: "success",
+                  amount: "+55%",
+                  label: "than last week",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <DefaultInfoCard
+                // color="success"
+                // icon=
+                title= "Total Requests"
+                description="Total no. of loan requests"
+                value="238"
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon="store"
+                title="Avg Credit Score"
+                count="34k"
+                percentage={{
+                  color: "success",
+                  amount: "+1%",
+                  label: "than yesterday",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon=""
+                title="Total Requests"
+                count="+91"
+                percentage={{
+                  color: "success",
+                  amount: "",
+                  label: "Just updated",
+                }}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default DashboardBank;
