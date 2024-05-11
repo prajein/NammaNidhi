3e5024cd904d9007307848fcf7776d930d3d1a30
prajein <prajeinck@gmail.com>
2024-05-11 15:44:16 +0530

    create a folder for mobile app
    along with a simple footer component

---
+++ src/examples/Items/NotificationItem/index.js
@@ -0,0 +1,27 @@
+import { forwardRef } from "react";
+import PropTypes from "prop-types";
+import MenuItem from "@mui/material/MenuItem";
+import Link from "@mui/material/Link";
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import menuItem from "examples/Items/NotificationItem/styles";
+
+const NotificationItem = forwardRef(({ icon, title, ...rest }, ref) => (
+  <MenuItem {...rest} ref={ref} sx={(theme) => menuItem(theme)}>
+    <MDBox component={Link} py={0.5} display="flex" alignItems="center" lineHeight={1}>
+      <MDTypography variant="body1" color="secondary" lineHeight={0.75}>
+        {icon}
+      </MDTypography>
+      <MDTypography variant="button" fontWeight="regular" sx={{ ml: 1 }}>
+        {title}
+      </MDTypography>
+    </MDBox>
+  </MenuItem>
+));
+
+NotificationItem.propTypes = {
+  icon: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default NotificationItem;
